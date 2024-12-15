f4d03cdd9125f391608e389d5e17aa7fe2cc33b8
Tushar Saxena <tushar.dec6@gmail.com>
2024-12-15 19:20:52 +0530

    Multer middleware created

---
+++ Backend/src/middleware/multer.middleware.js
@@ -0,0 +1,28 @@
+import multer from "multer";
+import fs from 'fs';
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/temp')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+
+const deleteFile = (filePath) => {
+    fs.unlink(filePath, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(`File deleted successfully`);
+        }
+    });
+};
+
+
+export const upload = multer({
+    storage,
+})
+
+export { deleteFile }
