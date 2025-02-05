7bdde846d6d7c367ed33a9f40f9c73684b82abdf
Tushar Saxena <tushar.dec6@gmail.com>
2025-02-06 02:10:08 +0530

    Created page one of home page

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './Navbar'
+
+import './Home.css'
 
 
 const Home = () => {
+
   return (
     <>
       <Navbar />
-      <div>
-        <h1>Home</h1>
+      <div id='home-page1'>
+        <h1 id="web-name">How'ztellerz</h1>
+        <div id='outer-webname-dot'>
+          <p id='webname-dot'></p>
+          <p>Scroll down</p>
+        </div>
       </div>
     </>
   )
