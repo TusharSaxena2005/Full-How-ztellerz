b8c6dec68273a7dcec8fee446fa0868e5d0903a3
Tushar Saxena <tushar.dec6@gmail.com>
2025-08-06 01:44:37 +0530

    Fixed a login bug

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -39,7 +39,7 @@ const App = () => {
                     element: <Home />
                 },
                 {
-                    path: "/signUp",
+                    path: "/signup",
                     element: <SignUp />
                 },
                 {
--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -18,8 +18,6 @@ const Login = () => {
         credentials: 'include'
       });
       if (response.ok) {
-        const data = await response.json();
-        console.log(data);
         window.location.href = '/home';
       }
     } catch (error) { }
