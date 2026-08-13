4bfa93f418dc15826dfbc276cde5ab3fd573116e
Tushar Saxena <tushar.dec6@gmail.com>
2026-08-13 20:35:51 +0530

    fixing otp issues

--- Backend/src/routes/mailer.route.js
+++ Backend/src/routes/mailer.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { sendOtpMail, contactUsMail } from "../controllers/mailer.controller.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const mailerRouter = Router();
 
-mailerRouter.post("/otpMail", sendOtpMail);
-mailerRouter.post("/contactUsMail", contactUsMail);
+mailerRouter.post("/otpMail", asyncHandler(sendOtpMail));
+mailerRouter.post("/contactUsMail", asyncHandler(contactUsMail));
 
 export { mailerRouter };
